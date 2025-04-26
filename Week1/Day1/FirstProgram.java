bd84e40404d07c3a3f9e263d541d2fa2a8c1b044
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-04-26 19:35:14 +0530

    Add files via upload

---
+++ Week1/Day1/FirstProgram.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+
+//access modifiers /keyword /classname
+public class FirstProgram {
+	
+	//type main and do ctrl+space
+	
+	public static void main(String[] args) {
+		
+		//type syso and do ctrl+space
+		System.out.println("helloworld");
+		
+		System.out.println("welcome to testleaf");
+		
+		//shortcut for execution--->ctrl+fn+f11
+		
+	}
+	
+
+}
---
+++ Week1/Day1/LearnDatatypes.java
@@ -0,0 +1,36 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		
+		//Integer
+		
+		byte age=32;
+		System.out.println(age);
+		
+		int noOfLearners=1002345678;
+		System.out.println(noOfLearners);
+		
+		long phNo=123456789L;
+		
+		short regNo=12345;
+		
+		float speed=3.657129734567f;
+		System.out.println(speed);
+		
+		double speed1=3.657129734567;
+		System.out.println(speed1);
+		
+		char logo='S';
+		
+		boolean isVisible=true;
+		
+		//Non primitive datatypes
+		String name="saranya";
+		
+		
+
+	}
+
+}
